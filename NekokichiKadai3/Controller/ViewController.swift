72dda75d3e349d184a8f097dacd46fb246ef9742
CatLuck2 <freeaddress834@gmail.com>
2022-02-28 20:07:20 +0900

    必要な機能を実装

--- NekokichiKadai3/Controller/ViewController.swift
+++ NekokichiKadai3/Controller/ViewController.swift
@@ -4,6 +4,11 @@
 //
 //  Created by Nekokichi on 2022/02/28.
 //
+/*
+ エラー処理
+ ・数値を入力したか
+ ・数値の型は正しいか
+ */
 
 import UIKit
 
@@ -14,22 +19,39 @@ class ViewController: UIViewController {
     @IBOutlet weak var num1Label: UILabel!
     @IBOutlet weak var num2Label: UILabel!
     @IBOutlet weak var calculatedResultLabel: UILabel!
+
+    private var calculateAdditionModel: CalculateAdditionModel = CalculateAdditionModel()
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBAction func inputNum1(_ sender: UITextField) {
+        calculateAdditionModel.num1 = Int(sender.text ?? "") ?? 0
+    }
+    
+    @IBAction func inputNum2(_ sender: UITextField) {
+        calculateAdditionModel.num2 = Int(sender.text ?? "") ?? 0
     }
     
     @IBAction func toggleSignOfNum1(_ sender: UISwitch) {
+        // 符号を反転
+        if case sender.isOn = true {
+            calculateAdditionModel.num1 = -abs(calculateAdditionModel.num1)
+        } else {
+            calculateAdditionModel.num1 = abs(calculateAdditionModel.num1)
+        }
     }
     
     @IBAction func toggleSignOfNum2(_ sender: UISwitch) {
+        // 符号を反転
+        if case sender.isOn = true {
+            calculateAdditionModel.num2 = -abs(calculateAdditionModel.num2)
+        } else {
+            calculateAdditionModel.num2 = abs(calculateAdditionModel.num2)
+        }
     }
     
     @IBAction func calculateAddition(_ sender: UIButton) {
+        num1Label.text = "\(calculateAdditionModel.num1)"
+        num2Label.text = "\(calculateAdditionModel.num2)"
+        calculatedResultLabel.text = "\(calculateAdditionModel.result())"
     }
-    
-    
-
 }
 
---
+++ NekokichiKadai3/Model/CalculateAdditionModel.swift
@@ -0,0 +1,18 @@
+//
+//  CalculateModel.swift
+//  NekokichiKadai3
+//
+//  Created by Nekokichi on 2022/02/28.
+//
+
+import UIKit
+
+struct CalculateAdditionModel {
+    var num1: Int = 0
+    var num2: Int = 0
+    
+    func result() -> Int {
+        num1 + num2
+    }
+    
+}
