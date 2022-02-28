2d352da52940ffdb844d8eac44a252b1f58a9048
CatLuck2 <freeaddress834@gmail.com>
2022-02-28 17:54:02 +0900

    UI部品を関連づけ

--- NekokichiKadai3/Controller/ViewController.swift
+++ NekokichiKadai3/Controller/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var inputNum1: UITextField!
+    @IBOutlet weak var inputNum2: UITextField!
+    @IBOutlet weak var num1Label: UILabel!
+    @IBOutlet weak var num2Label: UILabel!
+    @IBOutlet weak var calculatedResultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func toggleSignOfNum1(_ sender: UISwitch) {
+    }
+    
+    @IBAction func toggleSignOfNum2(_ sender: UISwitch) {
+    }
+    
+    @IBAction func calculateAddition(_ sender: UIButton) {
+    }
+    
+    
 
 }
 
