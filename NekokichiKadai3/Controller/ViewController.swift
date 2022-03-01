73dca230536a0fc458015eb35ad7e39d99aba0da
CatLuck2 <freeaddress834@gmail.com>
2022-03-01 14:08:43 +0900

    Modelにコメントを追加

--- NekokichiKadai3/Controller/ViewController.swift
+++ NekokichiKadai3/Controller/ViewController.swift
@@ -4,47 +4,39 @@
 //
 //  Created by Nekokichi on 2022/02/28.
 //
-/*
- エラー処理
- ・数値を入力したか
- ・数値の型は正しいか
- */
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    @IBOutlet weak var inputNum1: UITextField!
-    @IBOutlet weak var inputNum2: UITextField!
-    @IBOutlet weak var num1Label: UILabel!
-    @IBOutlet weak var num2Label: UILabel!
-    @IBOutlet weak var calculatedResultLabel: UILabel!
+final class ViewController: UIViewController {
+    @IBOutlet weak private var num1Label: UILabel!
+    @IBOutlet weak private var num2Label: UILabel!
+    @IBOutlet weak private var calculatedResultLabel: UILabel!
 
     private var calculateAdditionModel: CalculateAdditionModel = CalculateAdditionModel()
     
     @IBAction func inputNum1(_ sender: UITextField) {
-        calculateAdditionModel.num1 = Int(sender.text ?? "") ?? 0
+        calculateAdditionModel.setNum1(Int(sender.text ?? "") ?? 0)
     }
     
     @IBAction func inputNum2(_ sender: UITextField) {
-        calculateAdditionModel.num2 = Int(sender.text ?? "") ?? 0
+        calculateAdditionModel.setNum2(Int(sender.text ?? "") ?? 0)
     }
     
     @IBAction func toggleSignOfNum1(_ sender: UISwitch) {
         // 符号を反転
         if case sender.isOn = true {
-            calculateAdditionModel.num1 = -abs(calculateAdditionModel.num1)
+            calculateAdditionModel.setNum1(-abs(calculateAdditionModel.num1))
         } else {
-            calculateAdditionModel.num1 = abs(calculateAdditionModel.num1)
+            calculateAdditionModel.setNum1(abs(calculateAdditionModel.num1))
         }
     }
     
     @IBAction func toggleSignOfNum2(_ sender: UISwitch) {
         // 符号を反転
         if case sender.isOn = true {
-            calculateAdditionModel.num2 = -abs(calculateAdditionModel.num2)
+            calculateAdditionModel.setNum2(-abs(calculateAdditionModel.num2))
         } else {
-            calculateAdditionModel.num2 = abs(calculateAdditionModel.num2)
+            calculateAdditionModel.setNum2(abs(calculateAdditionModel.num2))
         }
     }
     
--- NekokichiKadai3/Model/CalculateAdditionModel.swift
+++ NekokichiKadai3/Model/CalculateAdditionModel.swift
@@ -7,12 +7,24 @@
 
 import UIKit
 
+// 値型であるstructが推奨されている
 struct CalculateAdditionModel {
-    var num1: Int = 0
-    var num2: Int = 0
+    // privateだと、取得と変更を行う関数を用意する必要がある
+    // 値の変更は影響度が高いので、関数で実装
+    private(set) var num1: Int = 0
+    private(set) var num2: Int = 0
     
+    // 引数には、num1と区別するために、value1を設定
+    mutating func setNum1(_ value1: Int) {
+        num1 = value1
+    }
+    
+    mutating func setNum2(_ value2: Int) {
+        num2 = value2
+    }
+    
+    // structの型名で足し算だと推測できるので、関数名はresult
     func result() -> Int {
         num1 + num2
     }
-    
 }
