aa31bde72a5b1f51e1477772648b4b9f6c773db2
CatLuck2 <freeaddress834@gmail.com>
2022-03-01 15:03:33 +0900

    他のコメントをついあk

--- NekokichiKadai3/Controller/ViewController.swift
+++ NekokichiKadai3/Controller/ViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 final class ViewController: UIViewController {
+    // 表示する値の変数名、UILabelのLabelだけをくっつけた
+    // StringやTextかは考慮しないので、含めなかった
     @IBOutlet weak private var num1Label: UILabel!
     @IBOutlet weak private var num2Label: UILabel!
+    // 計算結果を表示するだけなので、動詞は含めず、名詞を含めた
     @IBOutlet weak private var calculatedResultLabel: UILabel!
 
     private var calculateAdditionModel: CalculateAdditionModel = CalculateAdditionModel()
     
+    // 数値を入力し、Modelのプロパティへ格納するので、動詞のinputを含めた
+    // 引数のsenderでUITextFieldだと判別できるので、Fieldなどの単語は含めず
     @IBAction func inputNum1(_ sender: UITextField) {
         calculateAdditionModel.setNum1(Int(sender.text ?? "") ?? 0)
     }
@@ -22,9 +27,11 @@ final class ViewController: UIViewController {
         calculateAdditionModel.setNum2(Int(sender.text ?? "") ?? 0)
     }
     
+    // 数値の符号（英語：sign）を反転（英語：toggle）する
     @IBAction func toggleSignOfNum1(_ sender: UISwitch) {
-        // 符号を反転
+        // Switch文だとdefaultまで含めてしまうので、パターンマッチで実装
         if case sender.isOn = true {
+            // 絶対値を返す、abs()、を使用
             calculateAdditionModel.setNum1(-abs(calculateAdditionModel.num1))
         } else {
             calculateAdditionModel.setNum1(abs(calculateAdditionModel.num1))
@@ -32,7 +39,6 @@ final class ViewController: UIViewController {
     }
     
     @IBAction func toggleSignOfNum2(_ sender: UISwitch) {
-        // 符号を反転
         if case sender.isOn = true {
             calculateAdditionModel.setNum2(-abs(calculateAdditionModel.num2))
         } else {
@@ -40,7 +46,8 @@ final class ViewController: UIViewController {
         }
     }
     
-    @IBAction func calculateAddition(_ sender: UIButton) {
+    // 計算結果をUILabelで表示させるので、動詞のdisplayを使用
+    @IBAction func displayCalculatedResult(_ sender: UIButton) {
         num1Label.text = "\(calculateAdditionModel.num1)"
         num2Label.text = "\(calculateAdditionModel.num2)"
         calculatedResultLabel.text = "\(calculateAdditionModel.result())"
--- NekokichiKadai3/Model/CalculateAdditionModel.swift
+++ NekokichiKadai3/Model/CalculateAdditionModel.swift
@@ -7,10 +7,11 @@
 
 import UIKit
 
-// 値型であるstructが推奨されている
+// classよりも、値型であるstructが推奨されている
 struct CalculateAdditionModel {
     // privateだと、取得と変更を行う関数を用意する必要がある
-    // 値の変更は影響度が高いので、関数で実装
+    // 値の変更は影響度が高いので、変更のみを関数で実装
+    // 値の取得は影響度がないので、プロパティから取得可能にした
     private(set) var num1: Int = 0
     private(set) var num2: Int = 0
     
@@ -23,7 +24,7 @@ struct CalculateAdditionModel {
         num2 = value2
     }
     
-    // structの型名で足し算だと推測できるので、関数名はresult
+    // structの型名で足し算だと推測できるので、関数名はresultのみ
     func result() -> Int {
         num1 + num2
     }
