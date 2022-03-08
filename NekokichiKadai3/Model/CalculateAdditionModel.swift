b62c6b6846d77cfd8edf9990947523d08964bb48
CatLuck2 <freeaddress834@gmail.com>
2022-03-08 12:26:52 +0900

    諸々

--- NekokichiKadai3/Model/CalculateAdditionModel.swift
+++ NekokichiKadai3/Model/CalculateAdditionModel.swift
@@ -12,19 +12,28 @@ struct CalculateAdditionModel {
     // privateだと、取得と変更を行う関数を用意する必要がある
     // 値の変更は影響度が高いので、変更のみを関数で実装
     // 値の取得は影響度がないので、プロパティから取得可能にした
-    private(set) var num1: Int = 0
-    private(set) var num2: Int = 0
+    private(set) var signedNum1: Int = 0
+    private(set) var signedNum2: Int = 0
 
     // 引数には、num1と区別するために、value1を設定
-    mutating func setNum1(_ value1: Int) {
-        num1 = value1
+    mutating func input(
+        num1: Int,
+        isSwitch1On: Bool,
+        num2: Int,
+        isSwitch2On: Bool
+    ) {
+        signedNum1 = isSwitch1On ? -num1 : num1
+        signedNum2 = isSwitch2On ? -num2 : num2
     }
-    mutating func setNum2(_ value2: Int) {
-        num2 = value2
-    }
+//    mutating func setNum1(_ value1: Int) {
+//        num1 = value1
+//    }
+//    mutating func setNum2(_ value2: Int) {
+//        num2 = value2
+//    }
 
     // structの型名で足し算だと推測できるので、関数名はresultのみ
     func result() -> Int {
-        num1 + num2
+        signedNum1 + signedNum2
     }
 }
